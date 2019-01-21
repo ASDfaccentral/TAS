368501a86c8bfeda1bbad4c6c88d4ea71893f063
ASDfaccentral <45860414+ASDfaccentral@users.noreply.github.com>
2019-01-21 22:04:57 +0100

    Update code.c

--- code.c
+++ code.c
@@ -6,8 +6,16 @@ Write your code in this editor and press "Run" button to compile and execute it.
 
 *******************************************************************************/
 
-#include <stdio.h>
+#include<stdio.h>
+#include <time.h>
+#include <stdlib.h>
+#include <conio.h>
 #include <malloc.h>
+#include <limits.h> 
+
+
+
+
 
 typedef struct elt {
     int Nombre;
@@ -15,6 +23,152 @@ typedef struct elt {
     int *Tab;
     int type;
 }TAS;
+
+
+
+typedef int bool;
+#define true 1
+#define false 0
+
+
+typedef struct node
+{
+int info;
+struct node * left;
+struct node * right;
+}tree;
+
+typedef struct sNode 
+{ 
+  tree *t; 
+  struct sNode *next; 
+}pile; 
+
+
+/******************************************************************************
+                            Fonctions de bases sur pile pour arbre 
+*******************************************************************************/
+
+/* initialiser la pile */ 
+
+void Init_stack(pile **p)
+{
+    (*p)=NULL;
+}
+
+/* vérifier si la pile est vide */ 
+
+bool PisEmpty (pile *p)
+{
+    if (p==NULL) return (true); else return (false);
+}
+
+
+
+/* Fonction qui Empile un élement dans la pile */
+
+void push(pile ** Sommet, tree *t) 
+{ 
+  /* allocate tNode */
+  pile * new_tNode = 
+            (pile *) malloc(sizeof(pile)); 
+  
+  if(new_tNode == NULL) 
+  { 
+     printf("Stack Overflow \n"); 
+     getchar(); 
+     exit(0); 
+  }   
+ /* mettre l'@ de l'arbre  */
+  new_tNode->t  = t; 
+  
+  /* faire le chainage dans la pile  */
+  new_tNode->next = (*Sommet);    
+  
+  /* changer le sommet  */
+  (*Sommet)    = new_tNode; 
+} 
+
+/* Fonction qui depiler une élement de la pile*/
+tree *pop(pile ** Sommet) 
+{ 
+  tree *res; 
+  pile *top; 
+  
+  /*si la pile est vide afficher une erreur */
+  if(PisEmpty(*Sommet)) 
+  { 
+     printf("Stack Underflow \n"); 
+     getchar(); 
+     exit(0); 
+  } 
+  else
+  { 
+     top = *Sommet; 
+     res = (*Sommet)->t; 
+     *Sommet = top->next; 
+     free(top); 
+     return res; 
+  } 
+} 
+ 
+ /******************************************************************************
+                            Fonctions de bases sur arbre
+*******************************************************************************/
+
+
+bool IsEmpty( tree *T)
+{
+   if (T==NULL) return (true);
+   return (false);
+}
+
+
+tree * Left( tree *T)
+{
+if ( IsEmpty(T) )
+return NULL;
+else
+return T->left;
+}
+
+tree * Right( tree *T)
+{
+if ( IsEmpty(T) )
+return NULL;
+else
+return T->right;
+}
+
+bool IsLeave(tree *T)
+{
+if (IsEmpty(T))
+return false;
+else if ((IsEmpty(Left(T))) && (IsEmpty(Right(T))))
+return true;
+else
+return false;
+}
+
+
+bool IsInternalNode(tree *T)
+{
+return (! IsLeave(T) );
+}
+
+
+
+ 
+ 
+ 
+ 
+/******************************************************************************
+                            Fonctions de bases sur TAS
+*******************************************************************************/
+
+
+
+
 
 TAS * CreerTAS (int N, int type)
 {
@@ -103,6 +257,47 @@ TAS * ConstruireTAS ( int Temp[], int N )
 }
 
 
+tree * Rand_Tree()
+{   tree *T;
+   // time_t t;
+    
+   // srand((unsigned) time(&t));  // Initialization, should only be called once.
+   /* if (i==0) return NULL;
+         else { T=(tree *)malloc (sizeof(tree));
+                //if (i%2==0) {T->info= rand()%20; T->left=NULL; T->right=Rand_Tree(i-1);}
+                    // else {
+                    T->info= rand()%20; T->left=NULL; T->right=Rand_Tree(i-1); T->left=Rand_Tree(i-1);//}
+                      return (T);
+                  }*/
+    T=(tree *)malloc (sizeof(tree));
+    T->info =10;
+    T->left=(tree *)malloc (sizeof(tree));
+    T->left->info =5;
+    T->left->left=(tree *)malloc (sizeof(tree));
+    T->left->left->info =4;T->left->left->left=NULL; T->left->left->right=NULL;
+    T->left->right=(tree *)malloc (sizeof(tree));
+    T->left->right->info =6;T->left->right->left=NULL; T->left->right->right=NULL;
+    
+    T->right=(tree *)malloc (sizeof(tree));
+    T->right->info =15;
+    T->right->left=(tree *)malloc (sizeof(tree));
+    T->right->left->info =14;T->right->left->left=NULL; T->right->left->right=NULL;
+    T->right->right=(tree *)malloc (sizeof(tree));
+    T->right->right->info =16;T->right->right->left=NULL; T->right->right->right=NULL;
+    return (T);
+}
+
+void afficher (tree *T)
+{
+    if (!IsEmpty (T)) { printf ("%d  ", T->info );
+                        afficher(T->left);
+                        afficher(T->right);
+                        
+                              }
+}
+
+
+
 /***********************************************************************
  * EXox TD 
  * 
@@ -110,7 +305,54 @@ TAS * ConstruireTAS ( int Temp[], int N )
  * ******************************************************************/
 
 
+TAS * ABRtoTAS (tree *T)
+{
+  unsigned var=0;
+  pile *p;
+  tree *ABR;
+  
+  ABR =T;
+  
+  TAS *Ta=CreerTAS(30,1);
+  
+  Init_stack(&p);
+  push (&p, ABR); 
+  
+  while  (!PisEmpty(p))
+  {
+      ABR = pop (&p);
+      Inserer(Ta,ABR->info);
+      if (!IsEmpty(ABR->left)) push (&p, ABR->left); 
+      if (!IsEmpty(ABR->right)) push (&p, ABR->right); 
+      
+  }
+  
+  return (Ta);
+}
 
+
+int MinInTASMAX (TAS *T)
+{
+    int min,i; 
+    
+    min = T->Tab[T->Nombre-1];
+    for (i=T->Nombre-2;i>=(T->Nombre-1/2); i--)
+    {if (T->Tab[i]<min) min=T->Tab[i];}
+    return(min);
+}
+
+
+TAS * Deletekeme (TAS *T,  int k)
+{
+ if (T==NULL) return (NULL);
+ if ((k>=T->Nombre)||(k<0)) return(T);
+ if (k==T->Nombre-1){T->Nombre--; return (T);}
+ T->Tab[k]= T->Tab[T->Nombre-1];
+ T->Nombre--;
+ PercollerBAs(T,k);
+ return (T);
+    
+}
 
 
 
@@ -118,16 +360,32 @@ int main()
 {   int i, val;
    /** quesion 1 ***/ 
     TAS *T= CreerTAS(20,1);
-   
+    TAS *T2;
+    tree *ABR;
     for (i=0;i<9;i++) {//scanf("%d",&val);Inserer(T,val);
                        Inserer(T,i);}
-    for (i=0;i<T->Nombre-1; i++) printf("%d ",T->Tab[i]);
+    for (i=0;i<T->Nombre; i++) printf("%d ",T->Tab[i]);
     
     /*** question 3 **/
     val = Supprimer(T);
     
     printf ("\nla valeur suprimer est : %d\n",val);
-    for (i=0;i<T->Nombre-1; i++) printf("%d ",T->Tab[i]);
+    for (i=0;i<T->Nombre; i++) printf("%d ",T->Tab[i]);
+    
+    printf("\n Question 4 :\n Avant:  ");
+    ABR=Rand_Tree();
+    afficher(ABR);
+    printf("\n Aprés : ");
+    T2= ABRtoTAS(ABR);
+     for (i=0;i<T2->Nombre; i++) printf("%d  ",T2->Tab[i]);
+    
+    printf("\n Question 5: Le min est %d\n", MinInTASMAX(T2));
+    
+    printf("\n Question 6: Supprimer le 2 éme élement\n");
+    T2=Deletekeme(T2,2);
+    for (i=0;i<T2->Nombre; i++) printf("%d  ",T2->Tab[i]);
+    
+    printf ("\n Question 7: Trouvez le kéme élément plus petit dans un TASMAX .... TO be continued")
     
      printf("Hello World");
 
